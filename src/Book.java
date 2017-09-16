6338e1435b219d8b0da6dd4607a608391a3080f8
Rafal <Nurek_06@o2.pl>
2017-09-16 14:21:53 +0200

    Add setter and getter method.

--- src/Book.java
+++ src/Book.java
@@ -4,7 +4,33 @@ public class Book {
     private int ID;
     private String ISBN;
 
-    
+    public Book(String title, int ID, String ISBN) {
+        this.title = title;
+        this.ID = ID;
+        this.ISBN = ISBN;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
 }
